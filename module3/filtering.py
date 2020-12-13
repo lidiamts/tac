892b1db7051e7fa67d5a669426c4d87887e9614f
Lidia <lidia.mantesso@outlook.com>
2020-12-13 18:47:47 +0100

    TP3

--- module3/filtering.py
+++ module3/filtering.py
@@ -13,29 +13,27 @@ sw += ["les", "plus", "cette", "fait", "faire", "être", "deux", "comme", "dont"
        "ils", "bien", "sans", "peut", "tous", "après", "ainsi", "donc", "cet", "sous",
        "celle", "entre", "encore", "toutes", "pendant", "moins", "dire", "cela", "non",
        "faut", "trois", "aussi", "dit", "avoir", "doit", "contre", "depuis", "autres",
-       "van", "het", "autre", "jusqu", "ville"]
+       "van", "het", "autre", "jusqu", "conseil", "aucun", "doivent",
+       "nouvelle", "mois", "sieur", "très", "déja", "quatre", "cause", "agit", "grand", "également", "avant", "part", "donné",
+       "car", "quant", "parce", "alors", "moyen", "faite", "Leurs", "seulement", "dernier", "laquelle", "aucune", "aujourd'hui",
+       "ans", "nombre", "première", "plusieurs", "suite", "celles", "elles", "quand", "jour", "établi", "établi", "peu",
+       "ancien", "proposé", "peuvent", "seule", "toute", "beaucoup"]
 sw = set(sw)
 
 
-def filtering(year, folder=None):
-    if folder is None:
-        input_path = f"{year}.txt"
-        output_path = f"{year}_keywords.txt"
-    else:
-        input_path = f"{folder}/{year}.txt"
-        output_path = f"{folder}/{year}_keywords.txt"
-    output = open(output_path, "w", encoding='utf-8')
-    with open(input_path, encoding='utf-8') as f:
+def filtering(year):
+    path = f"{year}.txt"
+    output = open(f"{year}_keywords.txt", "w")
+
+    with open(path) as f:
         text = f.read()
         words = nltk.wordpunct_tokenize(text)
         kept = [w.lower() for w in words if len(
             w) > 2 and w.isalpha() and w.lower() not in sw]
         kept_string = " ".join(kept)
         output.write(kept_string)
-    return f'Output has been written in {output_path}!'
 
 
 if __name__ == '__main__':
     data_path = sys.argv[1]
-    chosen_year = sys.argv[2]
-    filtering(data_path, chosen_year)
+    filtering(year)
--- module3/s2_wordcloud.sh
+++ module3/s2_wordcloud.sh
@@ -5,5 +5,5 @@
 YEAR=$1
 cat "D:/DOCUMENTS/data/txt/*_${YEAR}_*.txt" > module3/${YEAR}.txt
 python module3/filter.py $YEAR
-wordcloud_cli --text module3/${YEAR}_keywords.txt --imagefile module3/${YEAR}.png --width 2000 --height 1000
+wordcloud_cli --text module3/1850_keywords.txt --imagefile module3/1850.png --width 2000 --height 1000
 display module3/${YEAR}.png
